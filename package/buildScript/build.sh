53c1990a3cc7ea72b13b050e8bc8a5a4a1bbe846
Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>
2024-05-29 21:11:16 -0700

    add params

--- package/buildScript/build.sh
+++ package/buildScript/build.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 #Purpose: build script
 
+echo "List of all arg: $@"
+
+POSITIONAL_ARGS=()
+
+#-s for skip setup
+SKIP_SETUP=0
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -s|--skip_setup)
+      shift # past argument
+      SKIP_SETUP=1
+      ;;
+    -s|--searchpath)
+      SEARCHPATH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 RED='\033[0;31m'
 NC='\033[0m'
 function print_error() {
@@ -15,28 +46,33 @@ BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "BASEDIR = $BASEDIR"
 
 ########################## build environment setup - BEGIN ####################################
-BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $BASEDIR
-SETUPSCRIPT_PATH=$BASEDIR/../setupScript
-if [ -d "$SETUPSCRIPT_PATH" ]; then
-    echo "Setup: $SETUPSCRIPT_PATH"
-	pushd $SETUPSCRIPT_PATH
-    
-    chmod +x *.sh
-    sed -i -e 's/\r$//' *.sh
-    
-    #check distro
-    if test -f /etc/lsb-release; then
-        echo "ubuntu!"
-        source $SETUPSCRIPT_PATH/setup_ubuntu.sh || print_error
+if [ $SKIP_SETUP -eq 0 ]; then
+    echo "running setup"
+    BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+    echo $BASEDIR
+    SETUPSCRIPT_PATH=$BASEDIR/../setupScript
+    if [ -d "$SETUPSCRIPT_PATH" ]; then
+        echo "Setup: $SETUPSCRIPT_PATH"
+        pushd $SETUPSCRIPT_PATH
+        
+        chmod +x *.sh
+        sed -i -e 's/\r$//' *.sh
+        
+        #check distro
+        if test -f /etc/lsb-release; then
+            echo "ubuntu!"
+            source $SETUPSCRIPT_PATH/setup_ubuntu.sh || print_error
+        fi
+        if test -f /etc/redhat-release; then
+            echo "redhat!"
+            source $SETUPSCRIPT_PATH/setup_redhat.sh || print_error
+        fi
+        
+        popd
+        echo "Setup: Done"
     fi
-    if test -f /etc/redhat-release; then
-        echo "redhat!"
-        source $SETUPSCRIPT_PATH/setup_redhat.sh || print_error
-    fi
-    
-	popd
-    echo "Setup: Done"
+else
+    echo "skipped setup"
 fi
 ########################## build environment setup - END ####################################
 
