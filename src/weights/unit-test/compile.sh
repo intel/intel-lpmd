d9b52d43e5a86057ad52e2850ec5e2fd33b26500
Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>
2024-05-31 12:01:37 -0700

    add unit-test code

---
+++ src/weights/unit-test/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#purpose: unit-test power supply status
+
+gcc main.c ../power-supply-status.c
+
+#outputs a.out file
+#./a.out
---
+++ src/weights/unit-test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../weights_common.h"
+
+int main() {
+
+   //printf("Hello, World!");
+   is_ac_powered_power_supply_status();
+   
+   return 0;
+   
+}
