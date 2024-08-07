f17e42c522d39c5d3259ac4389e6b463e97b5f7d
Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>
2024-08-07 13:31:30 -0700

    rename tuned profile, update profile script, update bundle script to remove older license
    Signed-off-by: Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>

---
+++ package/tuned-profile/intel_energy_optimizer/script.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#Copyright (C) 2024 Intel Corporation
+#SPDX-License-Identifier: GPL-2.0-or-later
+
+. /usr/lib/tuned/functions
+
+start() {
+
+	service="intel_lpmd"
+	# is service installed
+	if [ -f "/etc/init.d/$service" ]; then
+		#start energy optimizer as service with default configuration file.	
+		#sudo systemctl enable $service
+		sudo systemctl start $service
+		return 0
+	fi
+
+    return 1
+}
+
+stop() {
+
+	service="intel_lpmd"
+	# is service active
+	sudo systemctl is-active $service
+	if [ $? = 0 ]; then
+		#Terminate
+		sudo dbus-send --system --dest=org.freedesktop.intel_lpmd --print-reply /org/freedesktop/intel_lpmd org.freedesktop.intel_lpmd.Terminate >/dev/null 2>&1 &
+		sudo systemctl stop intel_lpmd >/dev/null 2>&1 &
+		
+		#To disable the service from starting automatically
+		sudo systemctl disable intel_lpmd
+	fi
+	
+    return 0
+}
+
+process $@
