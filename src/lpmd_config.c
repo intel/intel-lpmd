9021d6576af61f3c4eb35ccb27fd4ea2c987fbdd
Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>
2024-09-05 14:17:09 -0700

    Bug Fix: Remove hardcoded value
    Fix wlt_type range check upper limit from 3 to WLT_INVALID
    
    Signed-off-by: Gomathinayagam, Vinod <vinod.gomathinayagam@intel.com>

--- src/lpmd_config.c
+++ src/lpmd_config.c
@@ -153,7 +153,7 @@ static void lpmd_parse_state(xmlDoc *doc, xmlNode *a_node, lpmd_config_state_t *
 static int validate_config_state(lpmd_config_t *lpmd_config, lpmd_config_state_t *state)
 {
 	if (lpmd_config->wlt_hint_enable || lpmd_config->wlt_proxy_enable) {
-		if (state->wlt_type >=0 && state->wlt_type <= 3)
+		if (state->wlt_type >=0 && state->wlt_type < WLT_INVALID)
 			state->valid = 1;
 	} else {
 		if ((state->enter_cpu_load_thres > 0 && state->enter_cpu_load_thres <= 100) ||
