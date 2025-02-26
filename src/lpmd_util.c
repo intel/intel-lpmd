intel/intel-lpmd

commit 024706f244ad69b5b6afe329efef344eb9095df4

Check for calloc failure
In the function parse_proc_stat() check for the return status of
calloc(), before using proc_stat_prev and proc_stat_cur.

Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

src/lpmd_util.c
@@ -108,8 +108,18 @@ static int parse_proc_stat(void)
 
 	if (!proc_stat_prev)
 		proc_stat_prev = calloc(sizeof(struct proc_stat_info), count);
+
+	if (!proc_stat_prev)
+		return 1;
+
 	if (!proc_stat_cur)
 		proc_stat_cur = calloc(sizeof(struct proc_stat_info), count);
+
+	if (!proc_stat_cur) {
+		free(proc_stat_prev);
+		proc_stat_prev = NULL;
+		return 1;
+	}
 
 	memcpy (proc_stat_prev, proc_stat_cur, size);
 	memset (proc_stat_cur, 0, size);

commit ef70fe21d5918694d4cc5b93b995c0cfa94b48a6

Fix file descriptor resource leaks on error return paths
Fix a couple of resource leaks on error return paths by closing
the opened file.

Fixes: 024706f244ad ("Check for calloc failure")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

src/lpmd_util.c
@@ -109,14 +109,17 @@ static int parse_proc_stat(void)
 	if (!proc_stat_prev)
 		proc_stat_prev = calloc(sizeof(struct proc_stat_info), count);
 
-	if (!proc_stat_prev)
+	if (!proc_stat_prev) {
+		fclose (filep);
 		return 1;
+	}
 
 	if (!proc_stat_cur)
 		proc_stat_cur = calloc(sizeof(struct proc_stat_info), count);
 
 	if (!proc_stat_cur) {
 		free(proc_stat_prev);
+		fclose (filep);
 		proc_stat_prev = NULL;
 		return 1;
 	}