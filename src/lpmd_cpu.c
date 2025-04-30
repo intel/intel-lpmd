d07c06c7ddab465ae82db8433af8eb19fd76bcad
Zhang Rui <rui.zhang@intel.com>
2025-04-30 14:51:43 +0800

    Add PantherLake CPU ID
    Add PantherLake CPU ID so that lpmd can be launched on PantherLake
    platforms.
    
    PantherLake platform specific config file will be added later.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- src/lpmd_cpu.c
+++ src/lpmd_cpu.c
@@ -882,6 +882,7 @@ static struct cpu_model_entry id_table[] = {
 		{ 6, 0xaa }, // Meteorlake
 		{ 6, 0xac }, // Meteorlake
 		{ 6, 0xbd }, // Lunarlake
+		{ 6, 0xcc }, // Pantherlake
 		{ 0, 0 } // Last Invalid entry
 };
 
